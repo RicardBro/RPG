333cf8acbc490b93ac171861b93be9e00883331f
Ricardo De Leone <rskaeltas@gmail.com>
2018-01-30 19:06:41 -0300

    mov,anim,colicion

---
+++ Assets/Scrips/Seguimiento_Camara.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguimiento_Camara : MonoBehaviour {
+
+    public GameObject seguir; 
+    
+	// Update is called once per frame
+	void FixedUpdate () {
+	 float posX = seguir.transform.position.x;
+     float posY = seguir.transform.position.y;
+
+     transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ Assets/Scrips/jugador.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// No te olvides que es Key Sensitive
+public class jugador : MonoBehaviour {
+
+    public float velocidad = 3f;
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 movimiento;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        movimiento = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+        if (movimiento != Vector2.zero)
+        {
+            anim.SetFloat("MovX", movimiento.x);
+            anim.SetFloat("MovY", movimiento.y);
+            anim.SetBool("Caminando", true);
+        } else {
+            anim.SetBool("Caminando", false);
+        }
+    }
+
+    private void FixedUpdate() {
+
+        rb2d.MovePosition(rb2d.position + movimiento * velocidad * Time.deltaTime);    
+    }
+
+
+
+
+
+
+}
